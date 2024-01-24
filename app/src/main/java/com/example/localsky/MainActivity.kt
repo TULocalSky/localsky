45a9d7115f74273808d799ed091485ae9d78aef9
Nicholas Rucinski <tur45021@temple.edu>
2024-01-24 13:30:51 -0500

    Revert "test"
    This reverts commit 37d2a59d4e5722b910e5eded4b791995c05ccb8f.

--- app/src/main/java/com/example/localsky/MainActivity.kt
+++ app/src/main/java/com/example/localsky/MainActivity.kt
@@ -2,12 +2,10 @@ package com.example.localsky
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
