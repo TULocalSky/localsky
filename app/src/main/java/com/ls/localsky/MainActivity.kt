e948050947edc4b59e5ab8d7f2fa1bdab35cf99d
HydroLink <nrucinski01@gmail.com>
2024-03-26 19:50:35 -0400

    added example url

--- app/src/main/java/com/ls/localsky/MainActivity.kt
+++ app/src/main/java/com/ls/localsky/MainActivity.kt
@@ -12,6 +12,8 @@ import com.ls.localsky.ui.theme.LocalSkyTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Example
+        //val url = "/forecast/$api_key/{lat_and_long_or_time}"
         setContent {
             LocalSkyTheme {
                 // A surface container using the 'background' color from the theme
