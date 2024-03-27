df3ca31f6c0187a3b3133ff3eead8612e9dcde2a
johalloran01 <65722241+johalloran01@users.noreply.github.com>
2024-03-27 19:35:45 -0400

    added app package, with router for navigation logic

--- app/src/main/java/com/ls/localsky/MainActivity.kt
+++ app/src/main/java/com/ls/localsky/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.ls.localsky.app.LocalSkyApp
 import com.ls.localsky.loginScreen.LoginScreen
 import com.ls.localsky.ui.theme.LocalSkyTheme
 
@@ -16,7 +17,7 @@ class MainActivity : ComponentActivity() {
         //Example
         //val url = "/forecast/$api_key/{lat_and_long_or_time}"
         setContent {
-            LoginScreen()
+            LocalSkyApp()
             }
         }
     }
