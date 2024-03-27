49f7b63d685c1a5550c8b845cc0e40a525207a8d
johalloran01 <65722241+johalloran01@users.noreply.github.com>
2024-03-27 14:57:49 -0400

    added francis color themes

--- app/src/main/java/com/ls/localsky/ui/theme/Color.kt
+++ app/src/main/java/com/ls/localsky/ui/theme/Color.kt
@@ -9,3 +9,26 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Cherry Red
+val CherryRed50 = Color(0xFFd12e46) //light
+val CherryRed37 = Color(0xFF9D2235) //base
+
+//yellow
+val yellow50 = Color(0xFFFFCD00) //base
+val yellow30 = Color(0xFF997a00) //dark
+
+val metalicSilver = Color(0xFF8A8D8F)
+val silver = Color(0xFFC1C6C8)
+val black = Color(0xFF000000)
+val white = Color(0xFFFFFFFF)
+
+/*** complimentary colors to Temple (Owls) Colors ***/
+
+//cherry red
+val compCherryRed50 = Color(0xFF2ED1B9)
+val compCherryRed37 = Color(0xFF0FF9D2)
+
+//yellow
+val compYellow50 = Color(0xFF0032FF)
+val compYellow30 = Color(0xFF001F99)
