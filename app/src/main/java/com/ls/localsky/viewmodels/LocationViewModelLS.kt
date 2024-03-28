7dd2e36a8fc6b82c79ee7afa4aa0ee9b80b67d5d
Nicholas Rucinski <tur45021@temple.edu>
2024-03-28 14:49:57 -0400

    added a way to update the users position

--- app/src/main/java/com/ls/localsky/viewmodels/LocationViewModelLS.kt
+++ app/src/main/java/com/ls/localsky/viewmodels/LocationViewModelLS.kt
@@ -1,4 +1,27 @@
 package com.ls.localsky.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.type.LatLng
+
 class LocationViewModelLS {
+
+    private val currentUserLocation by lazy{
+        MutableLiveData<LatLng>()
+    }
+
+    /**
+     *
+     */
+    fun getCurrentUserLocation(): LiveData<LatLng>{
+        return currentUserLocation
+    }
+
+    /**
+     *
+     */
+    fun setCurrentUserLocation(newLocation: LatLng){
+        currentUserLocation.value = newLocation
+    }
+
 }
--- app/src/main/java/com/ls/localsky/viewmodels/WeatherViewModelLS.kt
+++ app/src/main/java/com/ls/localsky/viewmodels/WeatherViewModelLS.kt
@@ -1,9 +1,13 @@
 package com.ls.localsky.viewmodels
 
+import androidx.lifecycle.MutableLiveData
+
 class WeatherViewModelLS {
 
-    
-
+    //Change int to Weather data later
+    private val fullWeatherData by lazy {
+        MutableLiveData<Int>()
+    }
 
 
 }
