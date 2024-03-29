fc7ca37ef8cf678949cb94b3cf88eb550d5c6700
Francis R Scallatino <FrancisRScallatino@gmail.com>
2024-03-28 22:44:38 -0400

    make minor changes; add MapType  property

--- app/src/main/java/com/ls/localsky/ui/screens/MapScreen.kt
+++ app/src/main/java/com/ls/localsky/ui/screens/MapScreen.kt
@@ -2,29 +2,42 @@ package com.ls.localsky.ui.screens
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.android.gms.maps.model.CameraPosition
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.rememberMarkerState
+
+const val MARKER_STATE = "marker state"
 
 @Composable
 fun MapScreen(
     latitude: Double = 39.9528,
-    longitude: Double = 75.1635
+    longitude: Double = -75.1635
 ){
-    val cityHall = LatLng(latitude, longitude)
-    val cityHallState = MarkerState(position = cityHall)
+    val cityHall = remember{ LatLng(latitude, longitude) }
+    val cityHallState = rememberMarkerState(MARKER_STATE, cityHall)
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(cityHall, 10f)
+        position = CameraPosition.fromLatLngZoom(cityHall, 12f)
+    }
+
+    val mapProperties by remember {
+        mutableStateOf(MapProperties(mapType = MapType.NORMAL))
     }
 
     GoogleMap (
         modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
+        cameraPositionState = cameraPositionState,
+        properties = mapProperties
     ) {
         Marker(
             state = cityHallState,
