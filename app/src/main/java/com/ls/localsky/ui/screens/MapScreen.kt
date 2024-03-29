331781baabace4022bd31a130416c8dda7e3426e
Francis R Scallatino <tup31915@temple.edu>
2024-03-28 23:14:01 -0400

    remove unused import

--- app/src/main/java/com/ls/localsky/ui/screens/MapScreen.kt
+++ app/src/main/java/com/ls/localsky/ui/screens/MapScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.android.gms.maps.model.CameraPosition
