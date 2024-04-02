eea6e66e8b273f190fa8af4a072ccbfa67832c5b
Nicholas Rucinski <tur45021@temple.edu>
2024-04-02 12:30:04 -0400

    incorporated the weather data class into the weather view model

--- app/src/main/java/com/ls/localsky/viewmodels/WeatherViewModelLS.kt
+++ app/src/main/java/com/ls/localsky/viewmodels/WeatherViewModelLS.kt
@@ -1,12 +1,22 @@
 package com.ls.localsky.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.ls.localsky.network.WeatherData
 
 class WeatherViewModelLS {
 
     //Change int to Weather data later
     private val fullWeatherData by lazy {
-        MutableLiveData<Int>()
+        MutableLiveData<WeatherData>()
+    }
+
+    fun getWeatherData(): LiveData<WeatherData>{
+        return fullWeatherData
+    }
+
+    fun updateWeatherData(newData: WeatherData){
+        fullWeatherData.value = newData
     }
 
 
