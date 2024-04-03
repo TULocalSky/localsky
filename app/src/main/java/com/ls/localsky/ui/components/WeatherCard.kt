d3881c5e4b9aa84500ab2a357f0ac1e4a42f9959
johalloran01 <joshuamohalloran@gmail.com>
2024-04-03 11:50:06 -0400

    added resource files to drawables for weather icons based on pirate api return statements

--- app/src/main/java/com/ls/localsky/ui/components/WeatherCard.kt
+++ app/src/main/java/com/ls/localsky/ui/components/WeatherCard.kt
@@ -1,7 +1,15 @@
 package com.ls.localsky.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WeatherCard(
@@ -17,7 +25,12 @@ fun WeatherCard(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
-        )
+        ){
+
+
+        }
+
     }
 
 }
+
--- app/src/main/java/com/ls/localsky/ui/screens/WeatherScreen.kt
+++ app/src/main/java/com/ls/localsky/ui/screens/WeatherScreen.kt
@@ -1,8 +1,40 @@
 package com.ls.localsky.ui.screens
 
 import androidx.compose.runtime.Composable
+import android.content.Context
+import android.graphics.drawable.Icon
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ls.localsky.DatabaseLS
+import com.ls.localsky.R
+import com.ls.localsky.ui.app.LocalSkyAppRouter
+import com.ls.localsky.ui.app.Screen
+import com.ls.localsky.ui.components.ButtonComponent
+import com.ls.localsky.ui.components.ClickableRegisterText
+import com.ls.localsky.ui.components.DividerTextComponent
+import com.ls.localsky.ui.components.NormalTextInput
+import com.ls.localsky.ui.components.PasswordInput
+import com.ls.localsky.ui.components.TitleText
 
 @Composable
 fun weatherScreen(){
 
 }
+
+
