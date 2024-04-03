8c88001faa124004d7dda5a711ca8d9b2e0f646e
Nicholas Rucinski <tur45021@temple.edu>
2024-04-03 12:48:50 -0400

    added an entity to reflect the data in the cache

--- app/src/main/java/com/ls/localsky/CacheLS.kt
+++ app/src/main/java/com/ls/localsky/CacheLS.kt
@@ -1,6 +1,21 @@
 package com.ls.localsky
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.ls.localsky.models.WeatherData
+
 class CacheLS {
 
 
 }
+
+
+
+@Entity(tableName = "LocalSky")
+data class Result(
+    @PrimaryKey()
+    val userID: String,
+    @ColumnInfo
+    val lastWeatherData: WeatherData
+)
