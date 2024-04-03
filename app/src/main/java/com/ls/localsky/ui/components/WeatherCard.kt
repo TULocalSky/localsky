a4dbb1c68f902c54bd86fb99fef1864991eafb67
johalloran01 <joshuamohalloran@gmail.com>
2024-04-03 10:41:27 -0400

    Added a WeatherScreen and a WeatherCard composable UI component.

--- app/src/main/java/com/ls/localsky/ui/components/WeatherCard.kt
+++ app/src/main/java/com/ls/localsky/ui/components/WeatherCard.kt
@@ -1,2 +1,23 @@
 package com.ls.localsky.ui.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WeatherCard(
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+){
+    Card(
+        backgroundColor = backgroundColor,
+        shape = RoundedCornerShape(10.dp),
+        modifier = modifier.padding(16.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+    }
+
+}
--- app/src/main/java/com/ls/localsky/ui/screens/WeatherScreen.kt
+++ app/src/main/java/com/ls/localsky/ui/screens/WeatherScreen.kt
@@ -1,2 +1,8 @@
 package com.ls.localsky.ui.screens
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun weatherScreen(){
+
+}
