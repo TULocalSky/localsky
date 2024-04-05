0242e9da29e13e50cda4085e92b9033de3cd2725
Nicholas Rucinski <tur45021@temple.edu>
2024-04-05 11:26:12 -0400

    created two functions to update and get weather data in the cache database

--- app/src/main/java/com/ls/localsky/CacheLS.kt
+++ app/src/main/java/com/ls/localsky/CacheLS.kt
@@ -1,21 +1,58 @@
 package com.ls.localsky
 
+import android.content.Context
 import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
 import androidx.room.Entity
+import androidx.room.Insert
 import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.ls.localsky.models.WeatherData
 
-class CacheLS {
 
+class CacheLS(
+    val context: Context
+) {
+    val cache = Room.databaseBuilder(
+        context,
+        CacheDB::class.java, "LocalSkyDatabase"
+    ).build()
+
+    fun getCachedWeatherData(userID: String): WeatherData{
+        return cache.weatherDataDao().findByUserID(userID).lastWeatherData
+    }
+
+    fun updateCachedWeatherData(userID: String, weatherData: WeatherData){
+        cache.weatherDataDao().insert(UserWeatherData(userID, weatherData))
+    }
 
 }
 
-
+@Database(entities = [UserWeatherData::class], version = 1)
+abstract class CacheDB : RoomDatabase(){
+    abstract fun weatherDataDao() : WeatherDataDao
+}
 
 @Entity(tableName = "LocalSky")
-data class Result(
+data class UserWeatherData(
     @PrimaryKey()
     val userID: String,
     @ColumnInfo
     val lastWeatherData: WeatherData
 )
+
+@Dao
+interface WeatherDataDao {
+    @Query("SELECT * FROM LocalSky WHERE userID LIKE :uid LIMIT 1")
+    fun findByUserID(uid: String): UserWeatherData
+
+    @Insert
+    fun insert(userWeatherData: UserWeatherData)
+
+    @Delete
+    fun delete(userWeatherData: UserWeatherData)
+}
