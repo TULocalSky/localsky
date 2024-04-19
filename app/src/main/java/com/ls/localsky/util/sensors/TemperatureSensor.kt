bdfd3f3432fd12a0bfb7d14d10222298fcb1216f
Francis R Scallatino <tup31915@temple.edu>
2024-04-19 16:55:53 -0400

    turn TemperatureSensor into a singleton object

--- app/src/main/java/com/ls/localsky/util/sensors/TemperatureSensor.kt
+++ app/src/main/java/com/ls/localsky/util/sensors/TemperatureSensor.kt
@@ -3,12 +3,41 @@ package com.ls.localsky.util.sensors
 import android.content.Context
 import android.content.pm.PackageManager
 import android.hardware.Sensor
+import java.lang.ref.WeakReference
 
-class TemperatureSensor(
-    context: Context
+class TemperatureSensor private constructor(
+    private val context: Context
 ): AndroidSensor(
     context = context,
     sensorFeature = PackageManager.FEATURE_SENSOR_AMBIENT_TEMPERATURE,
     sensorType = Sensor.TYPE_AMBIENT_TEMPERATURE
 ) {
+    companion object{
+
+        //create a weak reference to avoid memory leaks
+        private var instanceReference: WeakReference<TemperatureSensor>? = null
+
+        fun getInstance(context: Context): TemperatureSensor{
+            val instance = instanceReference?.get()
+            return instance?: synchronized(this){
+                val newInstance = instanceReference?.get()
+                newInstance ?: TemperatureSensor(context.applicationContext).also {
+                    instanceReference = WeakReference(it)
+                }
+            }
+        }
+    }
+    var temp = Float.MAX_VALUE
+    fun getTempC(): Float{
+        this.startListening()
+        this.setOnSensorValuesChangedListener { temp = it[0] }
+        this.stopListening()
+        return temp
+    }
+    fun getTempF(): Float{
+        this.startListening()
+        this.setOnSensorValuesChangedListener { temp = (it[0]*(9/5)+32) }
+        this.stopListening()
+        return temp
+    }
 }
