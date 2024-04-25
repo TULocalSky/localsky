fbf37e5d49e4805063287e7650dcc0d98c53e14e
Nicholas Rucinski <tur45021@temple.edu>
2024-04-25 10:33:29 -0400

    created tests for parsing the time and checking if the location is in the range

--- app/src/test/java/com/ls/localsky/UtilsTest.kt
+++ app/src/test/java/com/ls/localsky/UtilsTest.kt
@@ -1,4 +1,46 @@
 package com.ls.localsky
 
+import com.ls.localsky.models.UserReport
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 class UtilsTest {
+
+    @Test
+    fun parseTimeTest(){
+        val time = "23/04/2024 12:14"
+        val parsedTime = parseTime(time)
+        val expected = "12:14 PM"
+        assertEquals(expected, parsedTime)
+    }
+
+    @Test
+    fun isLocationValidTest(){
+        val userReport = UserReport(
+            createdTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")),
+            latitude = 39.98157993,
+            longitude = -75.15689202
+        )
+        val isRight = isReportValidLocation(
+            userReport.latitude!!,
+            userReport.longitude!!,
+            userReport.latitude!! + .001,
+            userReport.longitude!! -0.001
+        )
+        val isWrong = isReportValidLocation(
+            userReport.latitude!!,
+            userReport.longitude!!,
+            userReport.latitude!! + 1,
+            userReport.longitude!! -1
+        )
+
+        assert(isRight)
+        assert(!isWrong)
+    }
+
+    fun isTimeValidTest(){
+
+    }
 }
